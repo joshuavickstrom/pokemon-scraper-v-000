b60da37534b423657ea190dfda0097a3a2dfc3fe
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-22 22:21:44 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -8,7 +8,7 @@ describe "Pokemon" do
     @sql_runner.execute_schema_migration_sql
   end
 
-  let(:pokemon) {Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db, hp: 60)}
+  let(:pokemon) {Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)}
 
   describe ".initialize" do
     it 'is initialized with keyword arguments of name, type and db' do
@@ -16,23 +16,22 @@ describe "Pokemon" do
       expect(pokemon).to respond_to(:name)
       expect(pokemon).to respond_to(:type)
       expect(pokemon).to respond_to(:db)
-      expect(pokemon).to respond_to(:hp)
     end
   end
 
   describe ".save" do
     it 'saves an instance of a pokemon with the correct id' do
-      Pokemon.save("Pikachu", "electric", @db, 60)
+      Pokemon.save("Pikachu", "electric", @db)
 
       pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
-      expect(pikachu_from_db).to eq([[1, "Pikachu", "electric", 60]])
+      expect(pikachu_from_db).to eq([[1, "Pikachu", "electric"]])
     end
   end
 
   describe ".find" do
     it 'finds a pokemon from the database by their id number and returns a new Pokemon object' do
       # The find method creates a new Pokemon after selecting their row from the database by their id number.
-      Pokemon.save("Pikachu", "electric", @db, 60)
+      Pokemon.save("Pikachu", "electric", @db)
 
       pikachu_from_db = Pokemon.find(1, @db)
       expect(pikachu_from_db.id).to eq(1)
