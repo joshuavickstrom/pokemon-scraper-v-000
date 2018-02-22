65cd8ce64cee7d0d45ede68cf75ddfdbb26a7049
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-22 22:17:08 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -8,7 +8,7 @@ describe "Pokemon" do
     @sql_runner.execute_schema_migration_sql
   end
 
-  let(:pokemon) {Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)}
+  let(:pokemon) {Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db, hp: 60)}
 
   describe ".initialize" do
     it 'is initialized with keyword arguments of name, type and db' do
@@ -16,15 +16,16 @@ describe "Pokemon" do
       expect(pokemon).to respond_to(:name)
       expect(pokemon).to respond_to(:type)
       expect(pokemon).to respond_to(:db)
+      expect(pokemon).to respond_to(:hp)
     end
   end
 
   xdescribe ".save" do
     it 'saves an instance of a pokemon with the correct id' do
-      Pokemon.save("Pikachu", "electric", @db)
+      Pokemon.save("Pikachu", "electric", @db, 60)
 
       pikachu_from_db = @db.execute("SELECT * FROM pokemon WHERE name = 'Pikachu'")
-      expect(pikachu_from_db).to eq([[1, "Pikachu", "electric"]])
+      expect(pikachu_from_db).to eq([[1, "Pikachu", "electric", 60]])
     end
   end
 
